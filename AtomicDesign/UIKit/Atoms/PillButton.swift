eac19a57093f7e3d89d66cb5c8fb7b9adab28e40
Pedja Jevtic <designbyheart@gmail.com>
2023-02-17 13:01:54 +0100

    Added icons

--- AtomicDesign/UIKit/Atoms/PillButton.swift
+++ AtomicDesign/UIKit/Atoms/PillButton.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class PillButton: UIButton {
+    
+    var titleText = "" {
+        didSet {
+            self.setTitle(titleText, for: .normal)
+            self.setTitle(titleText, for: .highlighted)
+            
+            self.setImage(UIImage(titleText), for: .normal)
+        }
+    }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setButton()
@@ -17,9 +27,18 @@ class PillButton: UIButton {
         self.frame.size.height = 40
         self.backgroundColor = .pillButtonColor
         self.layer.cornerRadius = frame.height / 2
-        self.setTitle("Izmeni", for: .normal)
         self.setTitleColor(UIColor.white, for: .normal)
+        self.setTitleColor(UIColor.black, for: .highlighted)
         self.sizeToFit()
+        
+        self.tintColor = .red
+    }
+    
+    func initWithFrame(_ title: String? = "", _ frame: CGRect) -> PillButton {
+        let pillButton = UIButton(frame: frame)
+        self.titleText = title ?? ""
+
+        return pillButton as! PillButton
     }
 }
  
--- AtomicDesign/ViewController.swift
+++ AtomicDesign/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var pillTest: PillButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pillTest.titleText = "Evo nasssss"
     }
-
-
+    
+    @IBAction func testBtn(_ sender: Any) {
+        pillTest.titleText = "Ma nemoj!"
+        icon.tintColor = .cancelButtonColor
+    }
 }
 
