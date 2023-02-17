f240f6ae6af30f7760967b7119e2b3210f7568c6
Pedja Jevtic <designbyheart@gmail.com>
2023-02-17 12:06:11 +0100

    Merged commit with colors

---
+++ AtomicDesign/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  AtomicDesign
+//
+//  Created by Predrag Jevtic on 2/17/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static func grayColor() -> UIColor? {
+        return UIColor(named: "grayColor")
+    }
+}
